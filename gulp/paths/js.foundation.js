6e02e32b361067f6ccde9ba014e56f35144f448b
odinokun <odinokun@gmail.com>
2018-05-03 12:20:12 +0300

    commit

---
+++ gulp/paths/js.foundation.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = [
+  './node_modules/jquery/dist/jquery.min.js',
+  './node_modules/svg4everybody/dist/svg4everybody.min.js',
+  // './src/vendors/smothScroll/smoothScroll.min.js',
+
+  // include Parallax
+  './node_modules/parallax-js/dist/parallax.min.js'
+
+  // include Slick slider
+  // './node_modules/slick-carousel/slick/slick.min.js',
+
+  // include fancybox3
+  // './node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js',
+];
